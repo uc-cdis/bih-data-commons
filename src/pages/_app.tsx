21db2c510c644ca868dc3d7c0745543630c45288
craigrbarnes <craig.barnes@intelligentlight.com>
2024-06-24 10:00:34 -0500

    alter config to new layout

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,115 +1,119 @@
-import App, { AppProps, AppContext, AppInitialProps } from 'next/app';
-import { useEffect } from 'react';
+import App, {AppProps, AppContext, AppInitialProps} from 'next/app';
+import {useEffect} from 'react';
 import {
-  Gen3Provider,
-  TenStringArray,
-  type ModalsConfig,
-  ContentSource,
-  RegisteredIcons,
-  Fonts,
-  SessionConfiguration,
+    Gen3Provider,
+    TenStringArray,
+    type ModalsConfig,
+    ContentSource,
+    RegisteredIcons,
+    Fonts,
+    SessionConfiguration,
 } from '@gen3/frontend';
 import '../styles/globals.css';
 import 'graphiql/graphiql.css';
 import '@graphiql/react/dist/style.css';
-import { GEN3_COMMONS_NAME, setDRSHostnames } from '@gen3/core';
+import {GEN3_COMMONS_NAME, setDRSHostnames} from '@gen3/core';
 import drsHostnames from '../../config/drsHostnames.json';
 
 interface Gen3AppProps {
-  colors: Record<string, TenStringArray>;
-  icons: RegisteredIcons;
-  themeFonts: Fonts;
-  modalsConfig: ModalsConfig;
-  sessionConfig: SessionConfiguration;
+    colors: Record<string, TenStringArray>;
+    icons: RegisteredIcons;
+    themeFonts: Fonts;
+    modalsConfig: ModalsConfig;
+    sessionConfig: SessionConfiguration;
 }
 
 const Gen3App = ({
-  Component,
-  pageProps,
-  colors,
-  icons,
-  themeFonts,
-  sessionConfig,
-  modalsConfig,
-}: AppProps & Gen3AppProps) => {
-  useEffect(() => {
-    setDRSHostnames(drsHostnames);
-  }, []);
+                     Component,
+                     pageProps,
+                     colors,
+                     icons,
+                     themeFonts,
+                     sessionConfig,
+                     modalsConfig,
+                 }: AppProps & Gen3AppProps) => {
+    useEffect(() => {
+        setDRSHostnames(drsHostnames);
+    }, []);
 
-  return (
-    <Gen3Provider
-      colors={colors}
-      icons={icons}
-      fonts={themeFonts}
-      sessionConfig={sessionConfig}
-      modalsConfig={modalsConfig}
-    >
-      <Component {...pageProps} />
-    </Gen3Provider>
-  );
+    return (
+        <Gen3Provider
+            colors={colors}
+            icons={icons}
+            fonts={themeFonts}
+            sessionConfig={sessionConfig}
+            modalsConfig={modalsConfig}
+        >
+            <DevSupport ComponentPreviews={ComponentPreviews}
+                        useInitialHook={useInitial}
+            >
+                <Component {...pageProps} />
+            </DevSupport>
+        </Gen3Provider>
+    );
 };
 
 // TODO: replace with page router
 Gen3App.getInitialProps = async (
-  context: AppContext,
+    context: AppContext,
 ): Promise<Gen3AppProps & AppInitialProps> => {
-  const ctx = await App.getInitialProps(context);
+    const ctx = await App.getInitialProps(context);
 
-  try {
-    const modals = await ContentSource.get(
-      `config/${GEN3_COMMONS_NAME}/modals.json`,
-    );
-    const session = await ContentSource.get(
-      `config/${GEN3_COMMONS_NAME}/session.json`,
-    );
+    try {
+        const modals = await ContentSource.get(
+            `config/${GEN3_COMMONS_NAME}/modals.json`,
+        );
+        const session = await ContentSource.get(
+            `config/${GEN3_COMMONS_NAME}/session.json`,
+        );
 
-    const fonts = await ContentSource.get(
-      `config/${GEN3_COMMONS_NAME}/themeFonts.json`,
-    );
+        const fonts = await ContentSource.get(
+            `config/${GEN3_COMMONS_NAME}/themeFonts.json`,
+        );
 
-    const themeColors = await ContentSource.get(
-      `config/${GEN3_COMMONS_NAME}/themeColors.json`,
-    );
+        const themeColors = await ContentSource.get(
+            `config/${GEN3_COMMONS_NAME}/themeColors.json`,
+        );
 
-    const colors = Object.fromEntries(
-      Object.entries(themeColors).map(([key, values]) => [
-        key,
-        Object.values(values) as TenStringArray,
-      ]),
-    );
+        const colors = Object.fromEntries(
+            Object.entries(themeColors).map(([key, values]) => [
+                key,
+                Object.values(values) as TenStringArray,
+            ]),
+        );
 
-    const icons = await ContentSource.get('config/icons/gen3.json');
+        const icons = await ContentSource.get('config/icons/gen3.json');
+        return {
+            ...ctx,
+            modalsConfig: modals,
+            sessionConfig: session,
+            themeFonts: fonts as Fonts,
+            colors: colors,
+            icons: icons as RegisteredIcons,
+        };
+    } catch (error: unknown) {
+        if (error instanceof Error) {
+            console.error('Provider Wrapper error loading config', error.toString());
+        } else console.error('Provider wrapper: unknown error');
+    }
+    // return default
     return {
-      ...ctx,
-      modalsConfig: modals,
-      sessionConfig: session,
-      themeFonts: fonts as Fonts,
-      colors: colors,
-      icons: icons as RegisteredIcons,
+        ...ctx,
+        colors: {},
+        themeFonts: {
+            heading: ['Poppins', 'sans-serif'],
+            content: ['Poppins', 'sans-serif'],
+            fontFamily: 'Poppins',
+        },
+        icons: {
+            prefix: 'gen3',
+            lastModified: 0,
+            icons: {},
+            width: 0,
+            height: 0,
+        },
+        modalsConfig: {},
+        sessionConfig: {},
     };
-  } catch (error: unknown) {
-    if (error instanceof Error) {
-      console.error('Provider Wrapper error loading config', error.toString());
-    } else console.error('Provider wrapper: unknown error');
-  }
-  // return default
-  return {
-    ...ctx,
-    colors: {},
-    themeFonts: {
-      heading: ['Poppins', 'sans-serif'],
-      content: ['Poppins', 'sans-serif'],
-      fontFamily: 'Poppins',
-    },
-    icons: {
-      prefix: 'gen3',
-      lastModified: 0,
-      icons: {},
-      width: 0,
-      height: 0,
-    },
-    modalsConfig: {},
-    sessionConfig: {},
-  };
 };
 export default Gen3App;
